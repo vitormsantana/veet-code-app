921d62a51ee42608ab4ea5143f5fe726259cfe53
vitormsantana <vitormsantana25@gmail.com>
2025-01-23 00:26:54 -0300

    refact: question form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuestionComponent } from './questions/question/question.component';
 import { QuestionsTableComponent } from './questions/questions-table/questions-table.component';
-
 import { HttpClientModule } from '@angular/common/http';
-
 import { FormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
+import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { QuestionsStatsComponent } from './questions/questions-stats/questions-stats/questions-stats.component';
 import { QuestionsStatsService } from './questions/questions-stats/questions-stats.service';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { ChartModule } from 'primeng/chart';
 import { StudyComponent } from './studies/study/study.component';
 import { HomeComponent } from './home/home.component';
@@ -56,7 +55,11 @@ import { QuestionsRecomendationsOpenaiComponent } from './questions/questions-re
     MatButtonModule,
     MatIconModule,
     HttpClientModule,
-  ],
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatSelectModule,
+    ReactiveFormsModule,  ],
   providers: [
     provideAnimationsAsync(),
     QuestionsStatsService
--- src/app/questions/question/question.component.ts
+++ src/app/questions/question/question.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-question',
@@ -9,12 +10,7 @@ import { HttpClient } from '@angular/common/http';
 })
 
 export class QuestionComponent {
-  question = {
-    name: '',
-    date: '',
-    difficulty: 'easy',
-    tags: [] as string[]
-  };
+  questionForm: FormGroup;
 
   availableTags = ['Arrays', 'String', 'Hash Tables', 'Linked Lists', 'Two Pointers', 'Sliding Window',
     'Stacks', 'Queues', 'Heaps', 'Binary Tree', 'BFS', 'DFS', 'Sets', 'Sort',
@@ -23,20 +19,38 @@ export class QuestionComponent {
   submittedQuestion: any = null;
   responseMessage: string = '';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.questionForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      difficulty: new FormControl('Easy', Validators.required),
+      date: new FormControl('', Validators.required),
+      tags: new FormControl([], Validators.required)
+    });
+  }
 
   submitForm() {
+    const question = { ...this.questionForm.value }; // Get the form data
+
+    // Format the date in dd/mm/yyyy format
+    question.date = this.formatDate(question.date);
+
     const apiUrl = 'https://6p4ojh18mj.execute-api.sa-east-1.amazonaws.com/dev/send_questions';
 
-    this.http.post(apiUrl, this.question).subscribe({
+    this.http.post(apiUrl, question).subscribe({
       next: (response: any) => {
         this.responseMessage = response.message || 'Question submitted successfully!';
-        this.submittedQuestion = { ...this.question };
+        this.submittedQuestion = { ...question };
       },
       error: (error) => {
         console.error('Error:', error);
         this.responseMessage = 'An error occurred while submitting the question.';
       }
     });
+  }
+
+  // Helper function to format the date
+  formatDate(date: string): string {
+    const [year, month, day] = date.split('-');
+    return `${day}/${month}/${year}`;
   }
 }
